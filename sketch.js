8c816de35b2e5fa1ac64b8f39f2a668acdf23943
barbajohnz <aab8666@nyu.edu>
2025-08-08 15:25:30 -0400

    version 2

--- sketch.js
+++ sketch.js
@@ -1,93 +1,159 @@
-// Object Class Practice:
-// Part 1: Write an object class that makes bouncing balls.
+let stickers = []; // array for bouncing stickers
+let glamEmojis = []; // array for floating glam emojis
+let button; // glam button
+let images = []; // loaded images
+let stickerOrder = []; // shuffled order, each image twice
+let nextIndex = 0; // track sticker index
+let surpriseImg; // animated gif for the end
 
-// Part 2: Change the design of the bouncing ball to something else (another shape? A combo of shapes? An image? Words? etc.).
-
-// Part 3: Make an array of 10 objects based on the class you wrote.
-
-// Part 4: Add a button. Program the button so that when you click on it, a new object will be added to the array.
-
-
-
-let hearts = []; //  array to hold multiple hearts
-let button; // button to add more hearts
-let colors; // array of possible heart colors
-let img1;
-
-
-function preload(){
-  img1 = loadImage('/Photos/subject1.png');
-
+function preload() {
+  // load subject images
+  for (let i = 0; i < 16; i++) {
+    let filename = `Photos/subject${i + 1}.png`;
+    images[i] = loadImage(filename);
+  }
 }
 
 function setup() {
-  createCanvas(600, 400);
-  noStroke();
-  // img1.resize(25,25);
-  image(img1, 0,0);
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  noCursor(); // hide default cursor
 
-  colors = [color("red"), color("blue"), color("yellow")];
-
-  // make 10 hearts to start
-  for (let i = 0; i < 10; i++) {
-    hearts.push(new Heart(random(width), random(height)));
+  // fill sticker order (each image twice)
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 16; j++) {
+      stickerOrder.push(j);
+    }
   }
+  shuffle(stickerOrder, true);
 
-  // add a heart when button is clicked
-  button = createButton("Add a Heart 💙❤️💛");
+  // create surprise gif in center
+  surpriseImg = createImg('Photos/surprise1.gif');
+  surpriseImg.position(width / 2, height / 2);
+  surpriseImg.size(min(windowWidth, windowHeight) / 2, min(windowWidth, windowHeight) / 2);
+  surpriseImg.style('transform', 'translate(-50%, -50%)');
+  surpriseImg.hide();
+
+  // glam button
+  button = createButton("Add a Drag Sticker 💄✨");
   button.position(20, 20);
-  button.mousePressed(() => {
-    hearts.push(new Heart(random(width), random(height)));
+  button.style('position', 'fixed');
+  button.style('font-size', '28px');
+  button.style('font-weight', 'bold');
+  button.style('color', '#ffffff');
+  button.style('background', 'linear-gradient(45deg, hotpink, deeppink, violet)');
+  button.style('border', 'none');
+  button.style('border-radius', '50px');
+  button.style('padding', '15px 30px');
+  button.style('box-shadow', '0 4px 15px rgba(0,0,0,0.3)');
+  // button.style('cursor', 'pointer');
+  button.style('transition', 'transform 0.2s ease');
+  button.mouseOver(() => {
+    button.style('transform', 'scale(1.1)');
   });
+  button.mouseOut(() => {
+    button.style('transform', 'scale(1)');
+  });
+  button.mousePressed(addSticker);
 }
 
 function draw() {
-  // background("white");
+  background(255);
 
-  // animate all hearts
-  for (let h of hearts) {
-    h.move();
-    h.display();
+  // update and display stickers
+  for (let s of stickers) {
+    s.move();
+    s.display();
+  }
+
+  // update and display glam emojis
+  for (let g of glamEmojis) {
+    g.move();
+    g.display();
+  }
+
+  // draw kiss emoji as custom cursor
+  textSize(60); // adjust cursor size
+  textAlign(CENTER, CENTER);
+  text("💋", mouseX, mouseY);
+}
+
+function addSticker() {
+  if (nextIndex < stickerOrder.length) {
+    // add sticker
+    let imgIndex = stickerOrder[nextIndex];
+    stickers.push(new ImageSticker(random(width), random(height), images[imgIndex]));
+
+    // add 2-3 floating glam emojis for fun
+    let emojis = ["💄", "🤡", "✨", "👑", "🌈", "💃", "🤖","🫦"];
+    let count = int(random(2, 4));
+    for (let i = 0; i < count; i++) {
+      glamEmojis.push(new GlamEmoji(random(emojis), random(width), random(height)));
+    }
+
+    nextIndex++;
+
+    // update button label if last sticker is next
+    if (nextIndex === stickerOrder.length) {
+      button.html("Click to see a Surprise Barbie 💅🎁");
+    }
+
+  } else if (nextIndex === stickerOrder.length) {
+    // show surprise gif
+    surpriseImg.show();
+    button.remove();
+    nextIndex++;
   }
 }
 
-//define the heart class
-class Heart {
-  constructor(x, y) {
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  if (surpriseImg) {
+    surpriseImg.position(width / 2, height / 2);
+    surpriseImg.size(min(windowWidth, windowHeight) / 2, min(windowWidth, windowHeight) / 2);
+  }
+}
+
+// bouncing sticker class
+class ImageSticker {
+  constructor(x, y, img) {
     this.x = x;
     this.y = y;
-    this.vx = random(-2, 2); // x movement speed
-    this.vy = random(-2, 2); // y movement speed
-    this.size = random(20, 40); // heart size
-    this.color = random(colors); // pick a color from the palette
+    this.vx = random(-2, 2);
+    this.vy = random(-2, 2);
+    this.img = img;
+    this.size = min(windowWidth, windowHeight) / 5;
   }
-
   move() {
-    // update position
     this.x += this.vx;
     this.y += this.vy;
-
-    // bounce off walls
     if (this.x < 0 || this.x > width) this.vx *= -1;
     if (this.y < 0 || this.y > height) this.vy *= -1;
   }
-
   display() {
-    fill(this.color);
-    this.drawHeart(this.x, this.y, this.size);
+    image(this.img, this.x, this.y, this.size, this.size);
   }
+}
 
-  // heart shape using two circles and a triangle
-  drawHeart(x, y, s) {
-    let r = s / 2;
-
-    // left circle
-    ellipse(x - r / 2, y - r / 2, r, r);
-
-    // right circle
-    ellipse(x + r / 2, y - r / 2, r, r);
-
-    // triangle tip
-    triangle(x - r, y - r / 4, x + r, y - r / 4, x, y + r);
+// floating glam emoji class
+class GlamEmoji {
+  constructor(char, x, y) {
+    this.char = char;
+    this.x = x;
+    this.y = y;
+    this.vx = random(-1, 1);
+    this.vy = random(-1, 1);
+    this.size = random(24, 48);
+  }
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x < 0 || this.x > width) this.vx *= -1;
+    if (this.y < 0 || this.y > height) this.vy *= -1;
+  }
+  display() {
+    textSize(this.size);
+    textAlign(CENTER, CENTER);
+    text(this.char, this.x, this.y);
   }
 }
